808d3ab0bcbc0aeb0ff024a46f3182bed1a89bd2
Pablo Bartolome <pbartolome@solstice-consulting.com>
2013-10-11 16:31:09 -0500

    BeconMap project

---
+++ BeaconMap/BeaconMap/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  BeaconMap
+//
+//  Created by Pablo Bartolome on 10/10/13.
+//  Copyright (c) 2013 Solstice Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) ViewController *mapViewController;
+
+@end
